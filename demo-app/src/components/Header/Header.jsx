0bb19ba7b9345f60c44212c419b689b86dd14842
hmao1 <35634135+hmao1@users.noreply.github.com>
2023-01-18 23:19:46 -0500

    initial setup: project structure

---
+++ demo-app/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+ import {Autocomplete} from '@react-google-maps/api';
+ import {AppBar, Toolbar, Typography, InputBase, Box} from '@mui/material';
+
+
+ /**
+  *
+  *
+  * @return {*} 
+  */
+ const Header = () => {
+    return(
+      <AppBar position="static">
+
+      </AppBar>
+    )
+ };
+
+ export default Header;
